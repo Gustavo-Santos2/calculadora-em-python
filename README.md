5b756ce0f31ceb96e8644e1bbad8afbc3590bd93
unknown <gusssantos012@gmail.com>
2024-08-09 12:45:46 -0300

    Adiciona script calculadora, atualiza README.md e .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# calculadora-em-python
+# 🧮 Calculadora em Python
+
+## 📚 Índice
+- [Sobre o Projeto](#sobre-o-projeto)
+- [Funcionalidades](#funcionalidades)
+- [Pré-requisitos](#pré-requisitos)
+
+- [Como Usar](#como-usar)
+- [Exemplos de Uso](#exemplos-de-uso)
+
+## 📖 Sobre o Projeto
+Este projeto apresenta uma calculadora intuitiva desenvolvida em Python. Seu principal objetivo é oferecer uma ferramenta eficiente para realizar operações aritméticas básicas, ao mesmo tempo que serve como um recurso educativo exemplar para quem deseja aprender programação em Python.
+
+## ✨ Funcionalidades
+- Adição
+- Subtração
+- Multiplicação
+- Divisão
+- Porcentagem
+- Operações com números inteiros e decimais
+- Interface de linha de comando
+
+## 🛠️ Pré-requisitos
+Antes de começar, certifique-se de ter o [Python](https://www.python.org/downloads/) instalado na sua máquina. Este projeto demonstra como criar uma calculadora moderna com uma interface gráfica utilizando a biblioteca [Flet](https://flet.dev/docs/), tornando-o uma excelente oportunidade para aprimorar suas habilidades em Python.
+
+## 🚀 Como Usar
+Para usar a calculadora, execute o script calculator.py:
+flet run calculadora.py
+
+## 📈 Exemplos de Uso
+
+#### Exemplo de adição
+- Escolha a operação (+, -, *, /): +
+- Insira o primeiro número: 10
+- Insira o segundo número: 5
+- Resultado: 10 + 5 = 15
+
+  ![image](https://github.com/Gustavo-Santos2/calculadora-em-python/assets/129700076/8351daad-2308-481b-9490-db0f34282784)
+  ![image](https://github.com/Gustavo-Santos2/calculadora-em-python/assets/129700076/8efd5869-ff4e-4357-bb9c-654449500edf)
+
+#### Exemplo de divisão
+- Escolha a operação (+, -, *, /): /
+- Insira o primeiro número: 10
+- Insira o segundo número: 2
+- Resultado: 10 / 2 = 5.0
+
+  ![image](https://github.com/Gustavo-Santos2/calculadora-em-python/assets/129700076/347e09c2-6e64-4b8f-a1b8-fd34feaf8de5)
+  ![image](https://github.com/Gustavo-Santos2/calculadora-em-python/assets/129700076/eeb99050-c7c6-41a3-892d-e3b2edcb8b1b)
---
+++ calculadora.py
@@ -0,0 +1,107 @@
+# pip install flet 
+# flet run calculadora.py 
+
+import flet as ft
+from flet import colors
+from decimal import Decimal
+
+OPERADORES = [
+    {'operador': 'AC', 'fonte': colors.BLACK, 'fundo': colors.BLUE_GREY_100},
+    {'operador': '±', 'fonte': colors.BLACK, 'fundo': colors.BLUE_GREY_100},
+    {'operador': '%', 'fonte': colors.BLACK, 'fundo': colors.BLUE_GREY_100},
+    {'operador': '/', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '7', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '8', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '9', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '*', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '4', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '5', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '6', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '-', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '1', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '2', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '3', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '+', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '0', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '.', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '=', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+]
+
+
+def calculate(operador, value_at):
+    try:
+        value = eval(value_at)
+
+        if operador == '%':
+            value /= 100
+        elif operador == '±':
+            value = -value
+    except:
+        return 'Error'
+
+    digits = min(abs(Decimal(value).as_tuple().exponent), 5)
+    return format(value, f'.{digits}f')
+
+
+def select(e, result):
+    value_at = result.value if result.value not in ('0', 'Error') else ''
+    value = e.control.content.value
+
+    if value.isdigit():
+        value = value_at + value
+    elif value == 'AC':
+        value = '0'
+    else:
+        if value_at and value_at[-1] in ('/', '*', '-', '+', '.'):
+            value_at = value_at[:-1]
+
+        value = value_at + value
+
+        if value[-1] in ('=', '%', '±'):
+            value = calculate(operador=value[-1], value_at=value_at)
+
+    result.value = value
+    result.update()
+
+
+def create_button(btn, result):
+    return ft.Container(
+        content=ft.Text(value=btn['operador'], color=btn['fonte']),
+        width=50,
+        height=50,
+        bgcolor=btn['fundo'],
+        border_radius=100,
+        alignment=ft.alignment.center,
+        on_click=lambda e: select(e, result)
+    )
+
+
+def main(page: ft.Page):
+    page.title = 'Calculadora'
+    page.bgcolor = '#000'
+    page.window_resizable = False
+    page.window_width = 270
+    page.window_height = 385
+    page.window_always_on_top = True
+
+    result = ft.Text(value="0", color=colors.WHITE, size=20)
+
+    display = ft.Row(
+        width=270,
+        controls=[result],
+        alignment='end',
+    )
+
+    buttons = [create_button(btn, result) for btn in OPERADORES]
+
+    keyboard = ft.Row(
+        width=270,
+        wrap=True,
+        controls=buttons,
+        alignment='end'
+    )
+
+    page.add(display, keyboard)
+
+
+ft.app(target=main)
